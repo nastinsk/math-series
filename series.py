0817290a3ab13d7fae063c63ad16479e5ba69a73
nastinsk <nastinsk@gmail.com>
2019-12-03 17:08:32 -0800

    tests fo lucas added, lucs function added

--- series.py
+++ series.py
@@ -1,10 +1,21 @@
 def fibonacci(n):
     previous = 0
     current = 1
+    try:
+        for i in range(n):
+            previous, current = current, previous + current
+        return previous
+    except TypeError:
+        return ("Input should be a one integer")
 
-    for i in range(n):
-        previous, current = current, previous + current
+def lucas(n):
+    previous = 2
+    current = 1
+    try:
+        for i in range(n):
+            previous, current = current, previous + current
+        return previous
+    except TypeError:
+        return ("Input should be a one integer")
 
-    return previous
-    
 
--- test_series.py
+++ test_series.py
@@ -1,4 +1,7 @@
-from series import fibonacci
+from series import fibonacci, lucas
+
+# Fibonacci tests"
+# Expected Outcome
 
 def test_zero():
     expected = 0
@@ -15,10 +18,58 @@ def test_15n():
     actual = fibonacci(15)
     assert actual == expected
 
-# expected failure
+# Expected Failure
 
 def test_negative():
     expected = 0
     actual = fibonacci(-4)
     assert actual == expected
+
+def test_letter():
+    expected = "Input should be a one integer"
+    actual = fibonacci("a")
+    assert actual == expected
+
+# Edge Case
+
+def test_float():
+    expected = "Input should be a one integer"
+    actual = fibonacci(1.354)
+    assert actual == expected
+
+
+# Lucas tests:
+# Expected Outcome
+def test_zero_lucas():
+    expected = 2
+    actual = lucas(0)
+    assert actual == expected
+
+def test_one_lucas():
+    expected = 1
+    actual = lucas(1)
+    assert actual == expected
+
+def test_three_lucas():
+    expected = 4
+    actual = lucas(3)
+    assert actual == expected
+
+def test_four_lucas():
+    expected = 7
+    actual = lucas(4)
+    assert actual == expected
+
+def test_15n_lucas():
+    expected = 1364
+    actual = lucas(15)
+    assert actual == expected
+
+# expected failure
+
+def test_negative_lucas():
+    expected = 2
+    actual = lucas(-4)
+    assert actual == expected
+
 
